2a91e3c76bec5d44db5c41081b327332ca32bc71
Timothy Hayes <tjhayes011@gmail.com>
2018-05-04 17:02:57 -0400

    viewmodel review create working

---
+++ LocalGourmet/LocalGourmet.PL/ViewModels/RRViewModel.cs
@@ -0,0 +1,14 @@
+﻿using LocalGourmet.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LocalGourmet.PL.ViewModels
+{
+    public class RRViewModel
+    {
+        public Restaurant Restaurant { get; set; }
+        public IEnumerable<Review> Reviews { get; set; }
+    }
+}
