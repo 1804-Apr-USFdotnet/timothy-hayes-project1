6f6f0a048d322da2a329b1ed65cb7d5b2553b0b0
Timothy Hayes <tjhayes011@gmail.com>
2018-05-05 14:16:41 -0400

    move script to Scripts folder

--- LocalGourmet/LocalGourmet.PL/App_Start/BundleConfig.cs
+++ LocalGourmet/LocalGourmet.PL/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace LocalGourmet.PL
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/TabCarousel.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ LocalGourmet/LocalGourmet.PL/Scripts/TabCarousel.js
@@ -0,0 +1,13 @@
+﻿function openPage(evt, pageName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("Page");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" btn-danger", "");
+    }
+    document.getElementById(pageName).style.display = "block";
+    evt.currentTarget.className += " btn-danger";
+}
