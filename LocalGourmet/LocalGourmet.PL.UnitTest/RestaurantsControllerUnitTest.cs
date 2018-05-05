dc343a3a6a3e3cca21cea4d8725bc5b3e79081fb
Timothy Hayes <tjhayes011@gmail.com>
2018-05-05 13:42:33 -0400

    Finish RestaurantsController UnitTests

--- LocalGourmet/LocalGourmet.PL.UnitTest/RestaurantsControllerUnitTest.cs
+++ LocalGourmet/LocalGourmet.PL.UnitTest/RestaurantsControllerUnitTest.cs
@@ -4,6 +4,7 @@ using System.Web.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LocalGourmet.BLL.Models;
 using LocalGourmet.PL.Controllers;
+using LocalGourmet.PL.ViewModels;
 
 namespace LocalGourmet.PL.UnitTest
 {
@@ -34,21 +35,65 @@ namespace LocalGourmet.PL.UnitTest
         [TestMethod]
         public void TestRestaurantsDetails()
         {
+            //Arrange
+            RestaurantsController controller = new RestaurantsController();
+
+            //Act
+            var result1 = controller.Details(1) as ViewResult;
+            var data1 = result1.Model as RestaurantDetailsVM;
+
+            var result2 = controller.Details(9) as ViewResult;
+            var data2 = result2.Model as RestaurantDetailsVM;
+
+            //Assert
+            Assert.IsNotNull(result1);
+            Assert.AreEqual("Subway", data1.Restaurant.Name);
+
+            Assert.IsNotNull(result2);
+            Assert.AreEqual("Hattricks", data2.Restaurant.Name);
         }
 
         [TestMethod]
         public void TestRestaurantsCreate()
         {
+            //Arrange
+            RestaurantsController controller = new RestaurantsController();
+
+            //Act
+            var result = controller.Create() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
         }
 
         [TestMethod]
         public void TestRestaurantsEdit()
         {
+            //Arrange
+            RestaurantsController controller = new RestaurantsController();
+
+            //Act
+            var result = controller.Edit(1) as ViewResult;
+            var data = result.Model as Restaurant;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(data.Name, "Subway");
         }
 
         [TestMethod]
         public void TestRestaurantsDelete()
         {
+            //Arrange
+            RestaurantsController controller = new RestaurantsController();
+
+            //Act
+            var result = controller.Delete(1) as ViewResult;
+            var data = result.Model as Restaurant;
+
+            //Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(data.Name, "Subway");
         }
     }
 }
