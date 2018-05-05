871eab7b030928f36b47745fac4f1b574c250b92
Timothy Hayes <tjhayes011@gmail.com>
2018-05-05 13:04:20 -0400

    Add UnitTest proj. for PL

---
+++ LocalGourmet/LocalGourmet.PL.UnitTest/HomeControllerUnitTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LocalGourmet.BLL.Models;
+using LocalGourmet.PL.Controllers;
+
+namespace LocalGourmet.PL.UnitTest
+{
+    [TestClass]
+    public class HomeControllerUnitTest
+    {
+        [TestMethod]
+        public void TestHomeHome()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Home() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void TestHomeAbout()
+        {
+        }
+
+        [TestMethod]
+        public void TestHomeContact()
+        {
+        }
+    }
+}
---
+++ LocalGourmet/LocalGourmet.PL.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LocalGourmet.PL.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LocalGourmet.PL.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("16b6d8ff-a3b8-4085-b89e-a3c34610a81d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
